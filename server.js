474cdaf45a8f0d6e0dd2dad725c6e61e02de910f
Roman Burlaka <burlakarv@gmail.com>
2023-04-20 17:08:46 +0200

    Dodany zmiany do  server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8081;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!');
+  res.end('Testowanie jest super SUPER! Nuner telefonu 666 222 293');
 }
 
 const server = http.createServer(requestListener);
